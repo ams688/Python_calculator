5699bc97bf814ded8baa5a62b7ac7d67266259c4
Ams688 <research0aman@gmail.com>
2026-02-15 14:10:32 +0530

    main logic & readme commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Python_calculator
 
-This is a calculator program that is designed to calculate (add/sub/multiply/divide). In also loops for continous calculation
+This is a calculator program that is designed to calculate (add/sub/multiply/divide). In also loops for continous calculation. I learnt to use the "__name__ == "__main__"" and this is really fun now I understand how to import works and why this name funtion is imp!!!
--- practice.py
+++ practice.py
@@ -1,59 +1,61 @@
-# Calculator Program
-print("\033[1mCalculator Program Welcome!\033[0m")
-#logic
-def add(*nums):
-    "This func is used to addition numaricals"
-    total = nums[0]
-    for num in nums[1:]:
-        total += num
-    return total
+def Calculator():
+    # Calculator Program
+    print("\033[1mCalculator Program Welcome!\033[0m")
+    #logic
+    def add(*nums):
+        "This func is used to addition numaricals"
+        total = nums[0]
+        for num in nums[1:]:
+            total += num
+        return total
 
-def sub(*nums):
-    "This func is used to subtract numaricals"
-    total = nums[0]
-    for num in nums[1:]:
-        total -= num
-    return total
+    def sub(*nums):
+        "This func is used to subtract numaricals"
+        total = nums[0]
+        for num in nums[1:]:
+            total -= num
+        return total
 
-def multiply(*nums):
-    "This func is used to multiply numaricals"
-    total = nums[0]
-    for num in nums[1:]:
-        total *= num
-    return total
+    def multiply(*nums):
+        "This func is used to multiply numaricals"
+        total = nums[0]
+        for num in nums[1:]:
+            total *= num
+        return total
 
-def division(*nums):
-    "This func is used to divide numaricals"
-    total = nums[0]
-    for num in nums[1:]:
-        total /= num
-    return total
+    def division(*nums):
+        "This func is used to divide numaricals"
+        total = nums[0]
+        for num in nums[1:]:
+            total /= num
+        return total
 
 
-while True:  
-    n1 = int(input("Enter a number: "))
-    n2 = int(input("Enter another number: "))
+    while True:  
+        n1 = int(input("Enter a number: "))
+        n2 = int(input("Enter another number: "))
 
-    #add
-    addition = add(n1, n2)
+        #add
+        addition = add(n1, n2)
 
-    #sub
-    substration = sub(n1, n2)
+        #sub
+        substration = sub(n1, n2)
 
-    #multiply
-    multiply_r = multiply(n1, n2)
+        #multiply
+        multiply_r = multiply(n1, n2)
 
-    #division
-    division_r = division(n1, n2)
+        #division
+        division_r = division(n1, n2)
 
-    #choose
-    choose = input("Choose the operation: ")
-    if choose == "Add".lower() or choose == "Add":
-        print(addition)
-    elif choose == "Sub".lower() or choose == "Sub":
-        print(substration)
-    elif choose == "Multiply".lower() or choose == "Multiply":
-        print(multiply_r)
-    elif choose == "Divide".lower() or choose == "Divide":
-        print(division_r)
-
+        #choose
+        choose = input("Choose the operation: ")
+        if choose == "Add".lower() or choose == "Add":
+            print(addition)
+        elif choose == "Sub".lower() or choose == "Sub":
+            print(substration)
+        elif choose == "Multiply".lower() or choose == "Multiply":
+            print(multiply_r)
+        elif choose == "Divide".lower() or choose == "Divide":
+            print(division_r)
+if __name__ == "__main__":  
+    Calculator()
